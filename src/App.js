3c22226629ac10e4ec35211179b29de959d3431b
Adam Parr <21086304+adamparr@users.noreply.github.com>
2022-11-23 07:11:17 +0000

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import CompoundInterestCalc from './components/CompoundInterestCalc';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
       <CompoundInterestCalc/>
     </div>
   );
